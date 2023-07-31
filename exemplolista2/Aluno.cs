0c54a8dc81e1aea62448ac3c8da5c8e81be0e501
Giovanna Simione <giovannasimionero@gmail.com>
2023-07-31 17:26:46 -0400

    first commit

---
+++ exemplolista2/Aluno.cs
@@ -0,0 +1,22 @@
+﻿public class Aluno
+{
+    public int Matricula { get; set; }
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public DateTime dataNascimento { get; set; }
+
+
+public Aluno()
+{
+
+}
+    public Aluno(int matricula, string nome, string cpf, DateTime dataNascimento)
+    {
+        this.Matricula = matricula;
+        this.Nome = nome;
+        this.Cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+}
+
---
+++ exemplolista2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+ public class Alas
+{
+    static void Main(string[] args)
+    {
+      
+        List<Aluno> alunos = new List<Aluno>();
+        string resposta;
+        
+        while(true) 
+        
+        {
+            Console.WriteLine("Deseja cadastrar um aluno?");
+            resposta = Console.ReadLine();
+            if (resposta == "sim")
+            {
+                Aluno a = new Aluno();
+                Console.WriteLine("digite o nome do aluno");
+                a.Nome = Console.ReadLine();
+
+                Console.WriteLine("digite a matricula do aluno");
+                a.Matricula = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("digite o cpf do aluno: ");
+                a.Cpf = Console.ReadLine();
+
+                Console.WriteLine("Digite a data de nascimento do aluno ");
+                a.dataNascimento = Convert.ToDateTime(Console.ReadLine());
+
+                alunos.Add(a);
+
+            }       
+            else if(resposta == "nao")
+            {
+                break;
+            }
+            
+        }
+
+        foreach (Aluno a in alunos)
+        {
+           Console.WriteLine("O nome é " + a.Nome + " a matricula"+ a.Matricula);
+
+       }
+
+        Console.ReadKey();
+    }
+}
+
+
+    
